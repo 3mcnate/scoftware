3a9e5e4c7aedf8b3d6c31ae08671b19fd265838c
Nate Boxer <nboxer@usc.edu>
2025-10-31 15:35:19 -0700

    add guide info columns

---
+++ supabase/migrations/20251031223500_add_guide_info.sql
@@ -0,0 +1,9 @@
+alter table "public"."guide_info" add column "emergency_contact_name" text not null;
+
+alter table "public"."guide_info" add column "emergency_contact_phone_number" text not null;
+
+alter table "public"."guide_info" add column "emergency_contact_relationship" text not null;
+
+alter table "public"."guide_info" add column "medical_history" text;
+
+
