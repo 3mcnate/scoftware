eaea875751ea286bf46a133d9329fcf782e44f10
Nate Boxer <nboxer@usc.edu>
2025-11-09 00:15:23 -0800

    email confirmation flow working

--- app/(auth)/auth/confirm/route.ts
+++ app/(auth)/auth/confirm/route.ts
@@ -1,7 +1,7 @@
 import { type EmailOtpType } from '@supabase/supabase-js'
 import { type NextRequest } from 'next/server'
 
-import { createClient } from '@/utils/supabase/server'
+import { createServerClient } from '@/utils/supabase/server'
 import { redirect } from 'next/navigation'
 
 export async function GET(request: NextRequest) {
@@ -11,7 +11,7 @@ export async function GET(request: NextRequest) {
   const next = searchParams.get('next') ?? '/'
 
   if (token_hash && type) {
-    const supabase = await createClient()
+    const supabase = await createServerClient()
 
     const { error } = await supabase.auth.verifyOtp({
       type,
@@ -21,8 +21,9 @@ export async function GET(request: NextRequest) {
       // redirect user to specified redirect URL or root of app
       redirect(next)
     }
+    redirect(`/auth-error?message=${error.message}`)
   }
 
   // redirect the user to an error page with some instructions
-  redirect('/error')
+  redirect(`/auth-error`)
 }
