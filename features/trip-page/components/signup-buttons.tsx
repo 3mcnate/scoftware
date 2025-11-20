9f9f648956ab237161264596f7e2b000589c78c2
Nate Boxer <nboxer@usc.edu>
2025-11-20 00:24:02 -0800

    add sign up button tooltips

--- features/trip-page/components/signup-buttons.tsx
+++ features/trip-page/components/signup-buttons.tsx
@@ -1,5 +1,14 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+import { useAuth } from "@/hooks/use-auth";
 import { cn } from "@/lib/utils";
+import { Car } from "lucide-react";
 
 interface SignupButtonsProps {
   className?: string;
@@ -7,6 +16,30 @@ interface SignupButtonsProps {
 }
 
 export function SignupButtons({ className, header }: SignupButtonsProps) {
+  const auth = useAuth();
+  if (auth.status === "authenticated") {
+    return (
+      <div
+        className={cn(
+          "flex flex-col sm:flex-row gap-2 justify-center items-stretch sm:items-center",
+          className
+        )}
+      >
+        <Button size="lg" className={cn({ "flex-1": header })}>
+          SIGN ME UP BIG TIME
+        </Button>
+        <Button
+          variant="secondary"
+          size="lg"
+          className={cn("bg-transparent", { "flex-1": header })}
+        >
+          <Car className="size-5" />
+          Driver Signup
+        </Button>
+      </div>
+    );
+  }
+
   return (
     <div
       className={cn(
@@ -14,16 +47,28 @@ export function SignupButtons({ className, header }: SignupButtonsProps) {
         className
       )}
     >
-      <Button size="lg" className={cn({ "flex-1": header })}>
-        SIGN ME UP BIG TIME!
-      </Button>
-      <Button
-        variant="outline"
-        size="lg"
-        className={cn("bg-transparent", { "flex-1": header })}
-      >
-        Driver Signup
-      </Button>
+      <Tooltip>
+        <TooltipTrigger asChild>
+          <Button size="lg" className={cn({ "flex-1": header })} >
+            SIGN ME UP BIG TIME
+          </Button>
+        </TooltipTrigger>
+        <TooltipContent>Sign in first to sign up!</TooltipContent>
+      </Tooltip>
+      <Tooltip>
+        <TooltipTrigger asChild>
+          <Button
+            variant="secondary"
+            size="lg"
+            className={cn("bg-transparent", { "flex-1": header })}
+            
+          >
+            <Car className="size-5" />
+            Driver Signup
+          </Button>
+        </TooltipTrigger>
+        <TooltipContent>Sign in first to sign up!</TooltipContent>
+      </Tooltip>
     </div>
   );
 }
