b1762858ee7de5096cf8eda6d86fbaf030b8e2b4
Nate Boxer <nboxer@usc.edu>
2025-11-28 12:23:08 -0800

    db migration

---
+++ supabase/migrations/20251128202145_pending_changes.sql
@@ -0,0 +1,15 @@
+grant delete on table "public"."allowed_trip_participants" to "postgres";
+
+grant insert on table "public"."allowed_trip_participants" to "postgres";
+
+grant references on table "public"."allowed_trip_participants" to "postgres";
+
+grant select on table "public"."allowed_trip_participants" to "postgres";
+
+grant trigger on table "public"."allowed_trip_participants" to "postgres";
+
+grant truncate on table "public"."allowed_trip_participants" to "postgres";
+
+grant update on table "public"."allowed_trip_participants" to "postgres";
+
+
