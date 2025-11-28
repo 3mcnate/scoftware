c5a271254d4e9caf4c3408c00224f099844fd993
Nate Boxer <nboxer@usc.edu>
2025-11-28 15:56:11 -0800

    Fixed supabase version type error?

--- data/participant/upsert-participant-info.ts
+++ data/participant/upsert-participant-info.ts
@@ -1,12 +1,11 @@
-import { createClient } from "@/utils/supabase/browser"
+import { createClient } from "@/utils/supabase/browser";
 import { useUpsertMutation } from "@supabase-cache-helpers/postgrest-react-query";
 
-export const useUpsertParticipantInfo() => {
+export const useUpsertParticipantInfo = () => {
 	const client = createClient();
 	return useUpsertMutation(
-		client.from('participant_info'),
-		['user_id'],
-		null
-	)
-	
-}
+		client.from("participant_info"),
+		["user_id"],
+		null,
+	);
+};
