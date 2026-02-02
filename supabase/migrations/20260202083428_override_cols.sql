88b3e17c1e58436f29d1f3ff3de555fab20e9063
Nate Boxer <nboxer@usc.edu>
2026-02-02 00:35:50 -0800

    almost forgot to supabase db diff

---
+++ supabase/migrations/20260202083428_override_cols.sql
@@ -0,0 +1,7 @@
+alter table "public"."trips" add column "driver_price_override" numeric;
+
+alter table "public"."trips" add column "member_price_override" numeric;
+
+alter table "public"."trips" add column "nonmember_price_override" numeric;
+
+
