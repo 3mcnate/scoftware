5604e4e1088d42ef2e029010160dfef4cdf55f8e
Nate Boxer <nboxer@usc.edu>
2026-02-05 12:37:23 -0800

    sb diff

---
+++ supabase/migrations/20260205020126_changes.sql
@@ -0,0 +1,15 @@
+grant delete on table "public"."stripe_products" to "postgres";
+
+grant insert on table "public"."stripe_products" to "postgres";
+
+grant references on table "public"."stripe_products" to "postgres";
+
+grant select on table "public"."stripe_products" to "postgres";
+
+grant trigger on table "public"."stripe_products" to "postgres";
+
+grant truncate on table "public"."stripe_products" to "postgres";
+
+grant update on table "public"."stripe_products" to "postgres";
+
+
